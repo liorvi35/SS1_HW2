aba593c452daf35d15b5d9bda15eefc1f4554c68
liorvi35 <liorvi35@gmail.com>
2022-12-04 20:19:20 +0200

    G
    updated

--- my_mat.h
+++ my_mat.h
@@ -1,5 +1,5 @@
 /**
- * this file contains the declarations of the fanctions from my_mat.h (i.e. its a libray)
+ * this file contains the declarations of the fanctions from my_mat.c and constants that will be in use (i.e. its a libray)
  * @version 1.0, 8 December 2022
  * @authors Lior Vinman (ID: 213081763) & Daniel Bespalov (ID: 213446479)
  */
@@ -9,7 +9,7 @@
 #define INF 10*10*10*10*10*10*10
 
 // defining functions
-int FW(int adj[10][10], int u, int v);
-void inputMat(int mat[10][10]);
-void isPathExists(int mat[10][10]);
-void shortestPath(int mat[10][10]);
+int FW(int[V][V], int, int);
+void inputMat(int[V][V]);
+void isPathExists(int[V][V]);
+void shortestPath(int[V][V]);
