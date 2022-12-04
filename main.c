40be9d3939cee0d2958dbefe6f37d4750ca660f2
liorvi35 <liorvi35@gmail.com>
2022-12-04 19:25:31 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /**
- * this file contains the main function
- * @version 1.0, 8 December 2022
- * @autors Lior Vinman (ID: 213081763) & Daniel Bespalov (ID: 213446479)
- */
+* this file contains the main function
+* @version 1.0, 8 December 2022
+* @autors Lior Vinman (ID: 213081763) & Daniel Bespalov (ID: 213446479)
+*/
 
 // including libraries
 #include <stdio.h>
@@ -11,13 +11,13 @@
 
 
 /**
- * this is the main function. it gets an character as input from the user and performs:
- *	A -> absorbs 100 (10x10) integers for the adjacency matrix of the graph
- *	B -> absorbs two integers that represents vertices, prints T if exists path between them, else prints F
- *	C -> absorbs two integers that represents vertices, prints the shortest path between them if exists, else prints -1
- *	D (and others, but we assume valid input) -> exits the program 
- * @return 0 iff entered D , -1 iff entered invalid input
- */
+* this is the main function. it gets an character as input from the user and performs:
+*	A -> absorbs 100 (10x10) integers for the adjacency matrix of the graph
+*	B -> absorbs two integers that represents vertices, prints T if exists path between them, else prints F
+*	C -> absorbs two integers that represents vertices, prints the shortest path between them if exists, else prints -1
+*	D (and others, but we assume valid input) -> exits the program 
+* @return 0 iff entered D , -1 iff entered invalid input
+*/
 int main()
 {
 	int adjMat[V][V] = {0};
@@ -39,7 +39,7 @@ int main()
 			case 'D':
 				return 0;
 				break;
-		}
+	}
 		printf("\n");
 		option = getchar();
 	}
